88912f649c74bbf71fb796657839ca01a4ad5f49
유상문 <sangmoon.yoo@wadiz.kr>
2022-10-26 21:52:58 +0900

    프로젝트 셋팅

---
+++ src/test/java/me/study/effectivejava/EffectiveJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package me.study.effectivejava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EffectiveJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
